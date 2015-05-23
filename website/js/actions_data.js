619c9cfb3e2f6f5de4f6038af188190fdeb5cf86
Beir Bear <beir.bear@gmail.com>
2015-05-23 13:18:01 +0200

    Add Action page (listview.html). I use this name for its general purpose list usage

---
+++ website/js/actions_data.js
@@ -0,0 +1,28 @@
+var dataContent = [
+    {   "id": 1,
+        "priority": 1,
+        "order": 1,
+        "tourName": "Elsen Tasarkhai",
+        "tourType": "short",
+        "description": "Elsen Tasarkhai is one of the most picturesque places of Mongolia. This 80 km long sand dune is located only 300 km away from Ulaanbaatar. The main road separates the dune into two parts, Mongol Els on the south and Khugnu Tarnyn Els on the north. The sand here is sodden under its surface, so shrubberies such as willow, elm and dogwood, grow in the area. Elsen Tasarkhai lies less than a hundred km away for Kharkhorin, or rather Karakorum, where Chinggis Khaan established a supply base in the mid-13th century. His son Ögedei ordered the construction of a proper capital, a decree that attracted traders, dignitaries and skilled workers from across Asia and even Europe.",
+        "itinerary": "Day 1. Ulaanbaatar - Elsen Tasarkhai Sand Dunes<br>In the morning yopu will meet your guide and driver, and drive west to the beautiful sand dunes of Elsen Tasarkhai. You will eat lunch in a ger camp. After that you will switch to horses and go for a tour to Erdene Khamba Temple located at the base of picturesque Khugnu Khan Mountain. In the evening you will have some free time to enjoy your time at the send dunes.<br>Overnight in a ger camp. L+D<br>Day 2. Elsen Tasarkhai - Kharkhorin - Ulaanbaatar<br>You will start your day with a hour long camel ride. Then you will leave Elsen Tasarkhai and drive to Kahrkhorin, a city which used to be the capital of Mongol Empire. In Kharkhorin our guide will take you to Erdene Zuu, one of Mongolia's oldest and largest monasteries, built in 16 century by Zanabazar, the frist Buddhist leader of Mongolia. From Kharkhorin you will drive back to Ulaanbaatar.<br>B+L",
+        "fare": "From 270 USD per person (budget tour)",
+        "info": "The price includes:Car, driver and fuel,English speaking guide,Accommodation,Meals,Drinking water,Entrance fees to national parks and monasteries,Horse / camel riding if mentioned in the itinerary.The price doesn't include:Personal shopping,Insurance,Alcoholic drinks,Activities, which are optional or not mentioned in the itinerary.",
+        "photoRef": "http://www.ttrmongolia.com/files/travel/f1adc274f5620eaa6e62482c222d1396.jpg",
+        "photoTitle": "Elsen Tasarkhai",
+        "alt": "Elsen Tasarkhai"
+    },
+    {   "id": 2,
+        "priority": 0,
+        "order": 2,
+        "tourName": "Gorkhi – Terelj National Park Trip",
+        "tourType": "short",
+        "description": "Gorkhi - Terelj National Park is one of the most popular tour destinations in Mongolia. This beautiful area lies only 70 km away from Ulaanbaatar and is a weekend run-away for both foreigners and Mongolians living in the city. Terelj National Park is located in one of the most stunningly beautiful natural wonders of Mongolia. There are spectacular valleys, rivers and forests, fascinating rock formations and caves. It is an ideal place for taking a trip on horseback, trekking along the Tuul River or visiting a local nomadic family. Only few kilometers away from Terelj you can find the 40 meter tall Chinggis Khan Equestrian Statue. Inside the statue there is a visitors centre and excellent bronze age and 13th century museum which are definitely worth the visit.",
+        "itinerary": "Day 1. Ulaanbaatar - Gorkhi-Terlji National Park<br>In the morning you will leave Ulaanbaatar and drive to Chinggis Khan Equestrian Statue, where you will visit a local museum and have a great opportunity to try wearing traditional Mongolian costume, deel. After that you will drive to Terelj National Park, where you will visit famous \"Turtle Rock\" and Aryabal Monastery. Later you will drive to a small camp owned by a local nomadic family, where you will eat lunch and rent horses for a ride. In the evening you will drive back to Ulaanbaatar. L",
+        "fare": "From 80 USD per person ( 1 day )",
+        "info": "T The price includes: <br> Car, driver and fuel, English speaking guide, Lunch, Drinking water, Entrance fee to Terelj National Park and Aryabal Monastery, Horse riding. The price doesn't include: Personal shopping, Insurance, Alcoholic drinks, Activities, which are optional or not mentioned in the itinerary.",
+        "photoRef": "http://www.ttrmongolia.com/files/travel/748ecb06f9050677227f0e94fea401c4.jpg",
+        "photoTitle": "Gorkhi",
+        "alt": "Gorkhi"
+    }
+];
---
+++ website/js/listView.js
@@ -0,0 +1,330 @@
+// Global Variable
+// dataContent is a variable that store all needed info
+var expandingStage;
+var searchIndex;
+var checkTourType;
+var filterState = false;
+
+// Constructor that initialize everything
+window.onload = function(){
+    // Initialize expanding stage
+    expandingStage  = new Array(dataContent.length)
+    searchIndex = new Array(dataContent.length);
+    
+    for(var i = 0; i < expandingStage.length; i++){
+        expandingStage[i] = "collapsed";
+        searchIndex[i] = true;
+    }
+    
+    // Add Event #searchKey
+    var txtSearchBox = document.getElementById("searchKey");
+    txtSearchBox.addEventListener("keypress", function(){
+        // Search for title
+        searchCondition();
+    })
+    
+    checkTourType = new Array();
+    checkTourType.push('short');
+    checkTourType.push('classic');
+    checkTourType.push('special');
+
+    
+    // Bind event to the filter button
+    document.getElementById("filterButton").addEventListener("click", function() {
+        if(filterState)
+        {
+            document.getElementById("filterPanel").style.display = "none";
+            this.innerHTML = "Show Filter";
+            filterState = false;
+            
+                
+            // Reset search
+            document.getElementById("searchKey").value = "";
+            document.getElementById('chkShort').checked = true;
+            document.getElementById('chkClassic').checked = true;
+            document.getElementById('chkSpecial').checked = true;
+            
+            for(var j = 0; j < dataContent.length; j++)
+                searchIndex[j] = true;
+            
+            redrawContent();
+        
+        }
+        else
+        {
+            document.getElementById("filterPanel").style.display = "block";
+            this.innerHTML = "Hide Filter";
+            filterState = true;
+        }    
+    });
+    // Hide Filter First as default //
+    document.getElementById("filterPanel").style.display = "none";
+    
+    redrawContent();
+}
+
+function searchCondition()
+{   
+    var txtSearchBox = document.getElementById("searchKey");    
+    var isChanged = false;
+    for(var j = 0; j < dataContent.length; j++){
+        if(txtSearchBox.value.trim().length > 0) 
+        {
+            console.log(checkTourType.indexOf(dataContent[j].tourType) > -1);
+            console.log(txtSearchBox.value.trim().toLowerCase());
+            if(dataContent[j].tourName.toLowerCase().contains(txtSearchBox.value.trim().toLowerCase()) && checkTourType.indexOf(dataContent[j].tourType) > -1 )
+            {
+                if(searchIndex[j] != true)
+                    isChanged = true;
+                searchIndex[j] = true;
+            }
+            else
+            {
+                if(searchIndex[j] != false)
+                    isChanged = true;
+                searchIndex[j] = false;   
+            }
+        }
+        else{
+            if(checkTourType.indexOf(dataContent[j].tourType) > -1 )
+            {
+                if(searchIndex[j] != true)
+                    isChanged = true;
+                searchIndex[j] = true;
+            }
+            else
+            {
+                if(searchIndex[j] != false)
+                    isChanged = true;
+                searchIndex[j] = false;   
+            }
+        }
+    }
+    if(isChanged)
+        redrawContent();
+}
+
+function chkUpdate(tourType)
+{
+    //var tourTypeCheckShort = true;
+    //var tourTypeCheckClassic = true;
+    //var tourTypeCheckSpecial = true;
+    checkTourType = new Array();
+    
+    if(tourType == 'Short' && document.getElementById('chk' + tourType).checked)
+        checkTourType.push('short');
+        //tourTypeCheckShort = document.getElementById('chk' + tourType).checked;
+    else if(tourType == 'Classic' && document.getElementById('chk' + tourType).checked)
+        checkTourType.push('classic');
+        //tourTypeCheckShort = document.getElementById('chk' + tourType).checked;
+    else if(tourType == 'Special' && document.getElementById('chk' + tourType).checked)
+        checkTourType.push('special');
+        //tourTypeCheckShort = document.getElementById('chk' + tourType).checked;
+
+    searchCondition();
+}
+
+// Redraw the content
+function redrawContent() {
+    // If dataContent is available, get the data and create an object
+    if(dataContent != null)
+    {
+        // Get container and clear stuffs inside
+        var listItemContainer = document.getElementById("filteredData");
+        listItemContainer.innerHTML = "";
+        
+        // Loop through every object
+        for(var i = 0; i < dataContent.length; i++)
+        {
+            if(!searchIndex[i])
+                continue;
+            
+            // Create outer container for margin only
+            var mComponent = document.createElement("div");
+            mComponent.setAttribute("style","margin: 0.25em");
+
+            // Create a main container
+            var component = document.createElement("div");
+            component.setAttribute("class","v960 col-sm-6");
+            component.setAttribute("id","expandable" + i);
+            
+            // Create a tour name (Title)
+            var titleComponent = document.createElement("a");
+            titleComponent.innerHTML = dataContent[i].tourName;
+            titleComponent.setAttribute("class","clickableText");
+            titleComponent.setAttribute("href","#");
+            
+            // Create a subcontainer that contain image and basic descriptino
+            var subComponent = document.createElement("div");
+            subComponent.setAttribute("class","subDetail limitHeight");
+            
+            // Create image
+            var imgSubComponent = document.createElement("img");
+            imgSubComponent.setAttribute("src", dataContent[i].photoRef);
+            imgSubComponent.setAttribute("alt", dataContent[i].alt);
+            imgSubComponent.setAttribute("title", dataContent[i].title);
+            imgSubComponent.setAttribute("class","imgFloatLeft");
+            subComponent.appendChild(imgSubComponent);
+            
+            // Create description
+            var txtSubComponent = document.createElement("p");
+            txtSubComponent.innerHTML = dataContent[i].description;
+            subComponent.appendChild(txtSubComponent);
+            
+            // Create read more button
+            var btnComponent = document.createElement("a");
+            btnComponent.setAttribute("onclick","expandItem(" + i + ")");
+            btnComponent.setAttribute("class", "clickableText");
+            btnComponent.innerHTML = "Read More";
+            
+            // insert these all parts into a main container
+            component.appendChild(titleComponent);
+            component.appendChild(subComponent);
+            component.appendChild(btnComponent);
+            
+            var specialComponent = null;
+            if(dataContent[i].priority == 1)
+            {
+                specialComponent = document.createElement("img");
+                specialComponent.setAttribute("src","http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-11/48/new-icon.png");
+                specialComponent.setAttribute("title","New");
+                specialComponent.setAttribute("class","pin");
+                component.appendChild(specialComponent);
+            }
+            mComponent.appendChild(component);
+            
+            // insert the master component into the main container
+            listItemContainer.appendChild(mComponent);
+        }
+    }
+    else
+    {
+        console.log("Cannot get data content")   
+    }
+}
+
+// Expand the item and inject information
+function expandItem(itemNum) {
+    
+    if (expandingStage[itemNum] == "collapsed") {
+        
+        // Reflag expand stage back to expand
+        expandingStage[itemNum] = "expand";
+        
+        // Get the main container
+        var tmp = document.getElementById("expandable" + itemNum);
+        tmp.setAttribute("class","v960 col-sm-12 autoHeight");
+        
+        var injContainer = tmp.getElementsByClassName("subDetail limitHeight")[0];
+        
+        if(injContainer != null)
+        {
+            injContainer.innerHTML = "";
+            
+            injContainer.setAttribute("class","subDetail");
+            
+            var imgSubComponent = document.createElement("img");
+            imgSubComponent.setAttribute("src", dataContent[itemNum].photoRef);
+            imgSubComponent.setAttribute("alt", dataContent[itemNum].alt);
+            imgSubComponent.setAttribute("title", dataContent[itemNum].title);
+            imgSubComponent.setAttribute("style", "width: 75%");
+            
+            var imgCenter = document.createElement("div");
+            imgCenter.setAttribute("style", "margin: 0.5em; text-align: center;");
+            imgCenter.appendChild(imgSubComponent);
+            
+            var tourType = document.createElement("p");
+            tourType.innerHTML = "<br><b>Tour Type</b>: " + dataContent[itemNum].tourType;
+            tourType.setAttribute("style", "margin: 0.5em");
+            
+            var tourDescription = document.createElement("p");
+            tourDescription.innerHTML = "<br><b>Description</b>:<br/>" + dataContent[itemNum].description;
+            tourDescription.setAttribute("style", "margin: 0.5em");
+            
+            var tourIti = document.createElement("p");
+            tourIti.innerHTML =  "<br><b>Itinerary</b><br>" + dataContent[itemNum].itinerary;
+            tourIti.setAttribute("style", "margin: 0.5em");
+
+            
+            var tourFair = document.createElement("p");
+            tourFair.innerHTML = "<br><b>Fair</b><br>" + dataContent[itemNum].fare;
+            tourFair.setAttribute("style", "margin: 0.5em");
+
+            
+            var tourInfo = document.createElement("p");
+            tourInfo.innerHTML = "<br><b>Other Information</b><br>" + dataContent[itemNum].info;
+            tourInfo.setAttribute("style", "margin: 0.5em");
+
+            
+            var txtSubComponent = document.createElement("p");
+            txtSubComponent.innerHTML = dataContent[itemNum].description;
+            txtSubComponent.setAttribute("style", "margin: 0.5em");
+
+            var clearComponent = document.createElement("div");
+            clearComponent.setAttribute("style", "cleat: both;");
+
+            var readMoreBtn = injContainer.parentNode.getElementsByClassName("clickableText");
+            readMoreBtn[1].innerHTML = "Collapse";
+            
+            injContainer.appendChild(imgCenter);
+            injContainer.appendChild(tourDescription);
+            injContainer.appendChild(tourType);
+            injContainer.appendChild(tourIti);
+            injContainer.appendChild(tourFair);
+            injContainer.appendChild(tourInfo);
+            injContainer.appendChild(txtSubComponent);
+            injContainer.appendChild(clearComponent);
+        }
+        else
+        {
+            console.log("Cannot get internal content");   
+        }
+        
+        /*
+        // Push Advertisement
+        var adDiv = document.createElement("div");
+        adDiv.setAttribute("id","adText");
+        adDiv.setAttribute("class","advertisement col-sm-6");    
+        adDiv.innerHTML = "<p>Advertisement</p>";
+        if(itemNum % 2 == 0){
+            document.getElementById("filteredData").insertBefore(adDiv, document.getElementById("expandable" + (itemNum - 1)));
+        console.log("expandable" + (itemNum - 1));
+        }
+        else
+        {
+            document.getElementById("filteredData").insertBefore(adDiv, tmp);
+            console.log(itemNum);
+        }
+        */
+    }
+    else if (expandingStage[itemNum] == "expand") {
+        // Change state back
+        expandingStage[itemNum] = "collapsed";
+
+        var tmp = document.getElementById("expandable" + itemNum);
+        tmp.setAttribute("class","v960 col-sm-6");
+        
+        var injContainer = tmp.getElementsByClassName("subDetail")[0];
+        injContainer.setAttribute("class","subDetail limitHeight");
+        injContainer.innerHTML = "";
+        
+        // Create image
+        var imgSubComponent = document.createElement("img");
+        imgSubComponent.setAttribute("src", dataContent[itemNum].photoRef);
+        imgSubComponent.setAttribute("alt", dataContent[itemNum].alt);
+        imgSubComponent.setAttribute("title", dataContent[itemNum].title);
+        imgSubComponent.setAttribute("class","imgFloatLeft");
+
+        // Create description
+        var txtSubComponent = document.createElement("p");
+        txtSubComponent.innerHTML = dataContent[itemNum].description;
+        
+        var readMoreBtn = injContainer.parentNode.getElementsByClassName("clickableText");
+        readMoreBtn[1].innerHTML = "Read More";
+        
+        // insert these all parts into a main container
+        injContainer.appendChild(imgSubComponent);
+        injContainer.appendChild(txtSubComponent);
+    }
+}
+
