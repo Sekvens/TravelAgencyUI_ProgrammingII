4c5798c142e73ac20e6ad51348a4d8467882d03b
John Shaw <j.j.g.shaw@gmail.com>
2015-06-01 07:59:46 +0200

    Added a script to resize picture dynamic depending on the parent div's size. Allows cleaner liquid design code and media queries code.

---
+++ website/js/global.js
@@ -0,0 +1,25 @@
+/*Put in this file small jqueries that are ran when the document is loaded. Might move the loader.js here as well.*/
+$(document).ready(function(){
+    /*Dynamic targeting of parent containers for rescaling*/
+    function rescalehelpfunction(selector) {
+        var $target = $(selector);
+        $target.each(function () {
+            var pheight = $(this).parent().height();
+            var pwidth = $(this).parent().width();
+            $(this).aeImageResize({ height: (pheight), width: (pwidth) });
+        })
+    }
+
+    function rescale(){
+        rescalehelpfunction(".resizeable");
+    }
+    /*Add a jquery tag of style target.resize()*/
+    jQuery.fn.extend({
+        rescale: function () {
+            return rescalehelpfunction(this);
+        }
+    });
+    /*Rescale the resizeable class when document is loaded*/
+    rescale();
+    
+});
